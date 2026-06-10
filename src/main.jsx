f37dbad21e2224ed60df36cb225426b8a462f815
Jonas Putman <jonas.putman@wur.nl>
2026-06-10 18:58:14 +0200

    fix: import App.css in main.jsx (was missing after rewrite)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './App.css'
 import App from './App.jsx'
 import { LangProvider } from './contexts/LangContext.jsx'
 
